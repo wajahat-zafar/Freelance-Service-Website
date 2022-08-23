210d759259b75c661851a95e556e10872bd397aa
Muhammad Wajahat <45312669+wajahat-zafar@users.noreply.github.com>
2022-08-23 07:32:54 -0700

    Add files via upload

---
+++ FreelanceServiceWeb/reactwebsite/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+import Home from './Components/Home'
+
+function App() {
+  return(
+    <React.Fragment>
+      <Header />
+      <Home />
+      <Footer />
+    </React.Fragment>
+
+  )
+
+}
+
+export default App;
---
+++ FreelanceServiceWeb/reactwebsite/src/Components/Home.js
@@ -0,0 +1,287 @@
+import React from "react";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+
+
+function Home(){
+    return(
+        <div style={{
+            width:"100%",
+            height:"100%",
+            margin: "0",
+            padding: "0",
+            overflowX:"hidden",
+            msoverflowStyle: "none"
+            
+          }}>
+          <div style={{backgroundColor: "#E5E7E9",
+            height:'1000px',
+            width:"100%",
+            }}>
+            <div style={{
+             
+              position: 'relative',
+              left: '200px',
+              top: '150px',
+              width:"100%",
+              height: '450px'
+          
+            }} >
+              <p style={{
+                    backgroundColor:"#9C9A03",
+                    width:"50px",
+                    height:'370px',
+                    position: "relative",
+                    top: '20px'
+          
+                  }} ></p>
+              <img src = "./images/image2.png" alt="" 
+              style={{
+                width:'280px',
+                height: '300px',
+                position: 'relative',
+                left: '18px',
+                bottom: '402px'
+              }}></img>
+              <Card style={{ width: '18rem',
+              backgroundColor:"#E5E7E9",
+              width: '300px',
+              height:'200px',
+              position: 'relative',
+              left: '265px',
+              bottom: '580px'}}>
+                <Card.Body>
+                  
+                  <p1 style={{
+                    color: 'gray',
+                    position: 'relative',
+                    left:'30px',
+                    top:'60px',
+                    textAlign:"center",
+                  
+                    fontWeight:'normal',
+                    fontSize: '19px' }}>WANT TO BECOME A BEE IN OUR NEST 
+                    GET YOURSELF SEEN BY OUR CLIENT WITH JUST A
+                    FEW CLICKS.</p1>
+                  <Button style={{
+                    backgroundColor: '#9C9A03',
+                    width: '200px',
+                    height:'48px',
+                    color:'white',
+                    position:'relative',
+                    left: '50px',
+                    top: '150px'
+                  }}>BECOME A FREELANCER</Button>
+                  
+                </Card.Body>
+              </Card>
+            </div>
+            <p style={{
+                    backgroundColor:"#9C9A03",
+                    width:"50px",
+                    height:'300px',
+                    position: "relative",
+                    top: '90px',
+                    left:'1400px'
+          
+                  }} ></p>
+                  <img src = "./images/image62.png"
+              style={{
+                width:'450px',
+                height: '300px',
+                position: 'relative',
+                left: '980px',
+                bottom: '182px'
+              }}></img>
+              <Card style={{ width: '18rem',
+              backgroundColor:"#E5E7E9",
+              width: '400px',
+              height:'300px',
+              position: 'relative',
+              left: '650px',
+              bottom: '450px'}}>
+                <Card.Body>
+                  
+                  <p1 style={{
+                    color: '#9C9A03',
+                    position: 'relative',
+                    
+                    top:'60px',
+                  
+                    fontWeight:'normal',
+                    fontSize: '19px' }}>THE PLACE WHERE YOU CAN FIND DEDICATED AND HARD WORKING PEOPLE, AS HARD AS BEES.HENCE,THE NAME.</p1>
+                  <Button style={{
+                    
+                    width: '200px',
+                    height:'48px',
+                    color:'gray',
+                    border: '5px solid #9C9A03',
+                    position:'relative',
+                    right: '100px',
+                    top: '150px'
+                  }}>FIND A FREELANCER</Button>
+                  
+                </Card.Body>
+              </Card>
+              <img style={{
+                width: "350px",
+                height: "400px",
+                position :"relative",
+                left: "590px",
+                bottom:"380px"
+              }} src='./images/quote1.png'></img>
+          
+              <div style={{
+                width: "100%",
+                height: "500px",
+                backgroundColor:"#FAD7A0 ",
+                position: "relative",
+                bottom: "440px"
+              }}>
+                <p style={{
+                  textAlign: "center",
+                  fontSize:"50px",
+                  position:"relative",
+                  top: "20px"
+                }}>WHAT TO EXPECTED AS A CLIENT</p>
+                <img  style={{
+                  position:"relative",
+                  left:"365px",
+                  top:"70px",
+                  width:"80px"
+                }} src='./images/vector.png' ></img>
+                <img style={{
+                  position:"relative",
+                  left:"280px",
+                  top:"120px",
+                  width:"90px"
+                }}src='./images/fb.png'></img>
+                <img style={{
+                  position:"relative",
+                  left:"160px",
+                  top:"240px",
+                  width:"160px",
+                  height: "100px"
+                }}src='./images/para.png'></img>
+                <img  style={{
+                  position:"relative",
+                  left:"365px",
+                  top:"70px",
+                  width:"80px"
+                }} src='./images/vector2.png' ></img>
+                <img style={{
+                  position:"relative",
+                  left:"280px",
+                  top:"120px",
+                  width:"90px"
+                }}src='./images/payment.png'></img>
+                <img style={{
+                  position:"relative",
+                  left:"160px",
+                  top:"240px",
+                  width:"160px",
+                  height: "100px"
+                }}src='./images/para2.png'></img>
+                <img  style={{
+                  position:"relative",
+                  left:"365px",
+                  top:"70px",
+                  width:"80px"
+                }} src='./images/vector3.png' ></img>
+                <img style={{
+                  position:"relative",
+                  left:"280px",
+                  top:"120px",
+                  width:"90px"
+                }}src='./images/available.png'></img>
+                <img style={{
+                  position:"relative",
+                  left:"160px",
+                  top:"240px",
+                  width:"160px",
+                  height: "100px"
+                }}src='./images/para3.png'></img>
+                
+              </div>
+              <div style={{
+                width: "100%",
+                height: "500px",
+                backgroundColor:"#17202A  ",
+                position: "relative",
+                bottom: "490px"
+              }}>
+                <p style={{
+                  textAlign: "center",
+                  color: "#9C9A03",
+                  fontSize:"50px",
+                  position:"relative",
+                  top: "40px"
+                }}>WHAT TO EXPECTED AS A FREELANCER</p>
+                <img  style={{
+                  position:"relative",
+                  left:"365px",
+                  top:"70px",
+                  width:"80px"
+                }} src='./images/vector4.png' ></img>
+                <img style={{
+                  position:"relative",
+                  left:"280px",
+                  top:"120px",
+                  width:"90px"
+                }}src='./images/job.png'></img>
+                <img style={{
+                  position:"relative",
+                  left:"160px",
+                  top:"240px",
+                  width:"160px",
+                  height: "100px"
+                }}src='./images/4.png'></img>
+                  <img  style={{
+                  position:"relative",
+                  left:"365px",
+                  top:"70px",
+                  width:"80px"
+                }} src='./images/vector5.png' ></img>
+                <img style={{
+                  position:"relative",
+                  left:"280px",
+                  top:"120px",
+                  width:"90px"
+                }}src='./images/safe.png'></img>
+                <img style={{
+                  position:"relative",
+                  left:"160px",
+                  top:"240px",
+                  width:"160px",
+                  height: "100px"
+                }}src='./images/para5.png'></img>
+                  <img  style={{
+                  position:"relative",
+                  left:"365px",
+                  top:"70px",
+                  width:"80px"
+                }} src='./images/vector6.png' ></img>
+                <img style={{
+                  position:"relative",
+                  left:"280px",
+                  top:"120px",
+                  width:"90px"
+                }}src='./images/add.png'></img>
+                <img style={{
+                  position:"relative",
+                  left:"160px",
+                  top:"240px",
+                  width:"160px",
+                  height: "100px"
+                }}src='./images/para6.png'></img>
+                
+              </div>
+            </div>
+            </div>
+          
+          
+    )
+}
+
+export default Home;
